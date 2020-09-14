ad67c84f5f21d9a8cbedb4ff92632058830e5527
Aayush <mahapatraaayush@gmail.com>
2020-09-14 12:51:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ import Router from './Router';
 const App = () => {
   useEffect(() => {
     const firebaseConfig = {
-      apiKey: 'AIzaSyAbsQu40YL3hG901Leyd7LvjKwZ935diNA',
-      authDomain: 'manager-5d75f.firebaseapp.com',
-      databaseURL: 'https://manager-5d75f.firebaseio.com',
-      projectId: 'manager-5d75f',
-      storageBucket: 'manager-5d75f.appspot.com',
-      messagingSenderId: '1012252017582',
-      appId: '1:1012252017582:web:4f582d4dc5d73ea7f63e3f'
+      
     };
     firebase.initializeApp(firebaseConfig);
   }, []);
